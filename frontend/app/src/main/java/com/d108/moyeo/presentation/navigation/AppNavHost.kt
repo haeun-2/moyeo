ba8385385c8fd185cd21dbead2dbbf130f113b52
김동찬 <kdc6208@naver.com>
2025-09-08 10:25:05 +0900

    chore: 빠져 있던 FirstScreen 임포트 추가

--- frontend/app/src/main/java/com/d108/moyeo/presentation/navigation/AppNavHost.kt
+++ frontend/app/src/main/java/com/d108/moyeo/presentation/navigation/AppNavHost.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.d108.moyeo.presentation.ui.screen.FirstScreen
 import com.d108.moyeo.presentation.ui.screen.exchange.ExchangeScreen
 import com.d108.moyeo.presentation.ui.screen.history.HistoryScreen
 import com.d108.moyeo.presentation.ui.screen.home.HomeScreen
