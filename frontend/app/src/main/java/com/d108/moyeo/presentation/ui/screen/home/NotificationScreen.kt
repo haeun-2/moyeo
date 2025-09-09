614f965ad7d21473bb8536842a99c1501d974b17
김동찬 <kdc6208@naver.com>
2025-09-09 17:56:13 +0900

    feat: 알림 화면에서 텍스트 헤더 추가

--- frontend/app/src/main/java/com/d108/moyeo/presentation/ui/screen/home/NotificationScreen.kt
+++ frontend/app/src/main/java/com/d108/moyeo/presentation/ui/screen/home/NotificationScreen.kt
@@ -1,10 +1,35 @@
 package com.d108.moyeo.presentation.ui.screen.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.d108.moyeo.presentation.theme.Padding
+import com.d108.moyeo.presentation.theme.Spacing
+import com.d108.moyeo.presentation.theme.Typography
 
 @Composable
 fun NotificationScreen(navController: NavController) {
-    Text("알림 화면")
+    Column(
+        modifier = Modifier.padding(
+            start = Spacing.Medium, end = Spacing.Medium,
+            top = Padding.ScreenTop, bottom = Padding.ScreenBottom
+        ),
+    ) {
+        // 제목
+        Box(
+            modifier = Modifier.fillMaxWidth(), // 가로 전체 차지
+            contentAlignment = Alignment.Center // 중앙 정렬
+        ) {
+            Text(
+                text = "알림",
+                style = Typography.titleLarge
+            )
+        }
+    }
 }
