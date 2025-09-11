dde9fac2f265441ae55fa43df66f046aaa1701c3
박상훈 <psh14@naver.com>
2025-09-11 13:44:54 +0900

    [FE][S13P21D108-82] design: 내 박스 상세 화면 디자인 수정

--- frontend/app/src/main/java/com/d108/moyeo/presentation/ui/screen/home/MyWalletScreen.kt
+++ frontend/app/src/main/java/com/d108/moyeo/presentation/ui/screen/home/MyWalletScreen.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Search
@@ -16,15 +16,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.d108.moyeo.presentation.theme.Spacing
 import com.d108.moyeo.presentation.theme.Typography
+import com.d108.moyeo.presentation.theme.button
+import com.d108.moyeo.presentation.theme.onPrimaryLight
 import com.d108.moyeo.presentation.ui.component.home.mywallet.MyWalletCurrencyBottomSheet
 import com.d108.moyeo.presentation.ui.component.home.mywallet.MyWalletFilterBottomSheet
 
@@ -61,47 +63,57 @@ fun MyWalletScreen(
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 onClick = { /* TODO: 충전하기 로직 */ },
-                icon = { Icon(Icons.Default.Add, "충전하기 아이콘") },
-                text = { Text(text = "충전하기") }
+                icon = { Icon(Icons.Default.Add, "충전 아이콘") },
+                text = { Text(text = "충전") }
             )
         },
         floatingActionButtonPosition = FabPosition.End
     ) { innerPadding ->
+        val layoutDir = LocalLayoutDirection.current
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding) // FAB에 가려지지 않도록 패딩 적용
-                .padding(horizontal = Spacing.Medium),
+                .padding(
+                    start = innerPadding.calculateLeftPadding(layoutDir),
+                    end = innerPadding.calculateRightPadding(layoutDir),
+                    bottom = innerPadding.calculateBottomPadding()
+                ), // FAB에 가려지지 않도록 패딩 적용
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // 상단 정보 카드
-            TopWalletInfoCard(
+            TopWalletInfoSurface(
                 walletName = uiState.walletName,
                 totalBalance = uiState.totalBalance,
                 onBalanceClick = viewModel::onBalanceClick
             )
 
-            // 검색 및 필터 바
-            SearchAndFilterBar(
-                searchQuery = uiState.searchQuery,
-                onSearchQueryChange = viewModel::onSearchQueryChanged, // 이벤트 연결
-                filters = uiState.filters,
-                onFilterClick = viewModel::onFilterClick
-            )
+            Column(modifier = Modifier.padding(horizontal = Spacing.Medium)) {
 
-            // 거래 내역 목록
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                items(uiState.transactions) { transaction ->
-                    TransactionRowItem(
-                        transaction = transaction,
-                        onClick = {
-                            // 클릭된 아이템의 id를 가지고 상세 화면으로 이동.
-                            navController.navigate("my_wallet_detail/${transaction.id}")
-                        })
-                    HorizontalDivider(color = Color.LightGray.copy(alpha = 0.5f))
+                // 검색 및 필터 바
+                SearchAndFilterBar(
+                    searchQuery = uiState.searchQuery,
+                    onSearchQueryChange = viewModel::onSearchQueryChanged, // 이벤트 연결
+                    filters = uiState.filters,
+                    onFilterClick = viewModel::onFilterClick
+                )
+
+                // 거래 내역 목록
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentPadding = PaddingValues(bottom = Spacing.Medium)
+                ) {
+                    items(uiState.transactions) { transaction ->
+                        TransactionRowItem(
+                            transaction = transaction,
+                            onClick = {
+                                // 클릭된 아이템의 id를 가지고 상세 화면으로 이동.
+                                navController.navigate("my_wallet_detail/${transaction.id}")
+                            })
+                        HorizontalDivider(color = Color.LightGray.copy(alpha = 0.5f))
+                    }
                 }
+
             }
         }
     }
@@ -109,50 +121,83 @@ fun MyWalletScreen(
 
 // 상단 정보 카드 UI
 @Composable
-private fun TopWalletInfoCard(
+private fun TopWalletInfoSurface(
     walletName: String,
     totalBalance: String,
     onBalanceClick: () -> Unit) {
-    Card(
+    Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp), // 높이를 200dp로 조정
-        shape = RoundedCornerShape(16.dp)
+            .height(300.dp), // 높이를 200dp로 조정
+        tonalElevation = 0.dp,
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(Spacing.Medium),
-            horizontalAlignment = Alignment.CenterHorizontally,  // 이 속성으로 수평 중앙 정렬
+//            horizontalAlignment = Alignment.CenterHorizontally,  // 이 속성으로 수평 중앙 정렬
             verticalArrangement = Arrangement.SpaceBetween  // 이 속성으로 영역 구분
         ) {
-            Text(
-                text = walletName,  // 여기에는 내 월렛의 이름이 떠야 함.
-                style = Typography.titleLarge
-            )
-
-            Row(  // 잔액이 보이는 영역
-                modifier = Modifier.clickable { onBalanceClick() },
-                verticalAlignment = Alignment.CenterVertically
+            Box(
+                modifier = Modifier.fillMaxWidth()
             ) {
+                IconButton(
+                    onClick = {/* TODO */},
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                        contentDescription = "뒤로가기"
+                    )
+                }
+
                 Text(
-                    text = totalBalance, // 임시 잔액
-                    style = Typography.displaySmall,
-                    fontWeight = FontWeight.Bold,
-                )
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = "화폐 선택"
+                    text = walletName,
+                    style = Typography.titleLarge,
+                    modifier = Modifier.align(Alignment.Center)
                 )
             }
 
+            Row(  // 잔액이 보이는 영역
+                modifier = Modifier
+                    .fillMaxWidth(),
+//                    .clickable { onBalanceClick() },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Row (
+                    modifier = Modifier
+                        .clickable(
+                            role = Role.Button,
+                            onClick = onBalanceClick,
+                        )
+                        .padding(Spacing.Small),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = totalBalance, // 임시 잔액
+                        style = Typography.displayLarge,
+                    )
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = "화폐 선택"
+                    )
+                }
+            }
+
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp) // 버튼 사이에 간격.
+                modifier = Modifier
+                    .fillMaxWidth(0.85f)
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.spacedBy(32.dp) // 버튼 사이에 간격.
                 // !!그리고 이 버튼들이 너무 크다. 좀 작아진 다음에 좌우와 간격이 있으면 좋겠는데. !!
             ) {
                 Button(
                     onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = button,
+                        contentColor = onPrimaryLight
+                    ),
                     modifier = Modifier.weight(1f), // 버튼이 남은 공간을 균등하게 차지하도록
                 ) {
                     Text("보내기")
@@ -160,6 +205,10 @@ private fun TopWalletInfoCard(
 
                 Button(
                     onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = button,
+                        contentColor = onPrimaryLight
+                    ),
                     modifier = Modifier.weight(1f) // 버튼이 남은 공간을 균등하게 차지하도록
                 ) {
                     Text("환전하기")
@@ -220,23 +269,33 @@ private fun SearchAndFilterBar(
 // 거래 내역 한 줄 UI
 @Composable
 private fun TransactionRowItem(transaction: Transaction, onClick: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = Spacing.Medium)
-            .clickable(onClick = onClick),
-        verticalAlignment = Alignment.CenterVertically
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = onClick,
+        color = Color.Transparent,
+        tonalElevation = 0.dp,
     ) {
-        Text(transaction.date, style = Typography.bodyMedium, color = Color.Gray)
-        Spacer(modifier = Modifier.width(Spacing.Medium))
-        Text(
-            text = transaction.description,
-            style = Typography.bodyLarge,
-            modifier = Modifier.weight(1f)
-        )
-        Column(horizontalAlignment = Alignment.End) {
-            Text(transaction.amount, style = Typography.bodyLarge, fontWeight = FontWeight.SemiBold)
-            Text(transaction.balance, style = Typography.bodySmall, color = Color.Gray)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = Spacing.Large),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(transaction.date, style = Typography.bodyMedium, color = Color.Gray)
+            Spacer(modifier = Modifier.width(Spacing.Medium))
+            Text(
+                text = transaction.description,
+                style = Typography.bodyLarge,
+                modifier = Modifier.weight(1f)
+            )
+            Column(horizontalAlignment = Alignment.End) {
+                Text(
+                    transaction.amount,
+                    style = Typography.bodyLarge,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(transaction.balance, style = Typography.bodySmall, color = Color.Gray)
+            }
         }
     }
 }
