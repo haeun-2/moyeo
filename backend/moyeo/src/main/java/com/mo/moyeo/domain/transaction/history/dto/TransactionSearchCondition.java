2ae435485e362301b0ca6982b7d3064ca3e145a4
김하은 <siji2135@naver.com>
2025-09-12 12:20:09 +0900

    [BE][S13P21D108-106] feat: 정산

--- backend/moyeo/src/main/java/com/mo/moyeo/domain/transaction/history/dto/TransactionSearchCondition.java
+++ backend/moyeo/src/main/java/com/mo/moyeo/domain/transaction/history/dto/TransactionSearchCondition.java
@@ -1,6 +1,7 @@
 package com.mo.moyeo.domain.transaction.history.dto;
 
 import com.mo.moyeo.domain.currency.entity.CurrencyType;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,7 +24,9 @@ public class TransactionSearchCondition {
     private Long categoryId;
     private CurrencyType currency;
 
+    @Min(value = 0, message = "page는 0 이상이어야 합니다.")
     private Integer page;
+    @Min(value = 1, message = "size는 1개 이상이어야 합니다.")
     private Integer size;
     private SortDirection sortDir;
 
--- backend/moyeo/src/main/java/com/mo/moyeo/domain/transaction/transfer/dto/TransferRequest.java
+++ backend/moyeo/src/main/java/com/mo/moyeo/domain/transaction/transfer/dto/TransferRequest.java
@@ -1,26 +1,28 @@
 package com.mo.moyeo.domain.transaction.transfer.dto;
 
 import com.mo.moyeo.domain.currency.entity.CurrencyType;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class TransferRequest {
 
-    @NotEmpty(message = "어떤 박스에서 출금할지 선택해주세요.")
+    @NotNull(message = "어떤 박스에서 출금할지 선택해주세요.")
     private Long fromBoxId;
 
-    @NotEmpty(message = "어떤 박스로 입금할지 선택해주세요.")
+    @NotNull(message = "어떤 박스로 입금할지 선택해주세요.")
     private Long toBoxId;
 
-    @NotEmpty(message = "이체할 통화를 선택해주세요.")
+    @NotNull(message = "이체할 통화를 선택해주세요.")
     private CurrencyType currency;
-    
-    @NotEmpty(message = "금액을 입력해 주세요.")
-    @Min(value = 0, message = "0보다 큰 수를 입력해주세요.")
+
+    @NotNull(message = "금액은 필수 입력값입니다.")
+    @Positive(message = "금액은 0보다 커야 합니다.")
     private Double amount;
 
 }
