a0f2a71867b9a2db016a4e24cb32978e3d70dcb3
이풍헌 <stly1974@gmail.com>
2025-09-12 00:43:58 +0000

    fix: 서버 기산을 한국 시간으로 설정

--- backend/moyeo/src/main/java/com/mo/moyeo/MoyeoApplication.java
+++ backend/moyeo/src/main/java/com/mo/moyeo/MoyeoApplication.java
@@ -3,13 +3,16 @@ package com.mo.moyeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import java.util.TimeZone;  // TimeZone import 추가
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class MoyeoApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(MoyeoApplication.class, args);
     }
 
 }
+
