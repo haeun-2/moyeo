776ccabe6868d02ac775835accb29a27c51fa204
박상훈 <psh4060144@gmail.com>
2025-09-16 12:34:59 +0900

    feat: Box 관련 DTO 분리

--- frontend/app/src/main/java/com/d108/moyeo/data/mapper/BoxMapper.kt
+++ frontend/app/src/main/java/com/d108/moyeo/data/mapper/BoxMapper.kt
@@ -1,12 +1,12 @@
 package com.d108.moyeo.data.mapper
 
 import com.d108.moyeo.data.remote.dto.box.BalanceDto
-import com.d108.moyeo.data.remote.dto.box.BoxDto
+import com.d108.moyeo.data.remote.dto.box.BoxResponseDto
 import com.d108.moyeo.domain.model.box.Balance
 import com.d108.moyeo.domain.model.box.Box
 import com.d108.moyeo.domain.model.box.BoxType
 
-fun BoxDto.toDomain() = Box(
+fun BoxResponseDto.toDomain() = Box(
     id = boxId,
     name = name,
     balances = balances.map(BalanceDto::toDomain),
--- frontend/app/src/main/java/com/d108/moyeo/data/remote/dto/box/BoxResponseDto.kt
+++ frontend/app/src/main/java/com/d108/moyeo/data/remote/dto/box/BoxResponseDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class BoxDto(
+data class BoxResponseDto(
     @SerialName("boxId")
     val boxId: Long,
 
@@ -16,13 +16,4 @@ data class BoxDto(
 
     @SerialName("type")
     val type: String, // "PERSONAL" | "GROUP"
-)
-
-@Serializable
-data class BalanceDto(
-    @SerialName("currency")
-    val currency: String,
-
-    @SerialName("balance")
-    val balance: Double
 )
