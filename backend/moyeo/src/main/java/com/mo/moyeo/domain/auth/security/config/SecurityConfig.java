85c49688a4b2042ce3a3ae0f9ee3da45c5bc74d4
이풍헌 <stly1974@gmail.com>
2025-09-18 13:18:19 +0900

    fix: SecurityConfig에 permitAll 제거

--- backend/moyeo/src/main/java/com/mo/moyeo/domain/auth/security/config/SecurityConfig.java
+++ backend/moyeo/src/main/java/com/mo/moyeo/domain/auth/security/config/SecurityConfig.java
@@ -40,8 +40,11 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/api/auth/signup/**", "/api/auth/login", "/api/auth/refresh", "/api/boxes/invite/*", "/download.html").permitAll()
-                        .requestMatchers(("/**")).permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/auth/signup/**", "/api/auth/login", "/api/auth/refresh").permitAll()
+                        .requestMatchers("/api/boxes/invite/**", "/download.html").permitAll()
+                        .requestMatchers("/api/payments").permitAll()
+//                        .requestMatchers(("/**")).permitAll()
                         .anyRequest().authenticated())
         ;
 
