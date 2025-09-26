3c3f352a3c41e391e479a8d57c7752bba19bc8df
김동찬 <kdc6208@naver.com>
2025-09-26 17:58:04 +0900

    [FE][S13P21D108-171] feat:  모임 박스 권한 수정 관련 API 연결

--- frontend/app/src/main/java/com/d108/moyeo/data/remote/api/BoxMemberService.kt
+++ frontend/app/src/main/java/com/d108/moyeo/data/remote/api/BoxMemberService.kt
@@ -1,11 +1,21 @@
 package com.d108.moyeo.data.remote.api
 
 import com.d108.moyeo.data.remote.dto.box.BoxMemberDto
+import com.d108.moyeo.data.remote.dto.box.UpdatePermissionRequestDto
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 
 interface BoxMemberService {
     @GET("api/boxes/{boxId}/members")
     suspend fun getBoxMembers(@Path("boxId") boxId: Long): Response<List<BoxMemberDto>>  // 리모트 DTO
+
+
+    @PATCH("api/boxes/{boxId}/members/permissions")
+    suspend fun updateBoxMemberPermission(
+        @Path("boxId") boxId: Long,
+        @Body requestList: List<UpdatePermissionRequestDto>
+    ): Response<Unit>
 }
